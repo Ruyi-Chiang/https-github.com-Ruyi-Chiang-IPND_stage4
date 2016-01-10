9f686ba9a06eb6fb12d71b1991cf58c44f6d2706
Ruyi Chiang <ruyi.chiang@gmail.com>
2016-01-10 13:26:53 +0800

    docs: Add comment validation function to the form (User must input comment = Good to be known as valid)

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,54 @@
 import webapp2
+import os
+import jinja2
+
+# Set up jinja environment
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+form = '''
+<form method="post">
+    Leave your comments. I would appreciate. :)
+    <br>
+    <input type="text" name="comment" value="%(comment)s">
+    <br>
+    %(error)s
+    <br>
+    <input type="submit">
+</form>
+'''
+
+# Comment validation function
+def valid_comment(comment):
+    if comment != "Good":
+        return True
 
 
 class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+    def write_form(self, error="", comment=""):
+        self.response.out.write(form % {"comment": comment, "error": error})
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-], debug=True)
+    def get(self):
+        # self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.out.write(form)
+        self.write_form()
+
+    def post(self):
+        user_input = self.request.get("comment")
+        if valid_comment(user_input):
+            self.write_form("That's not a valid input.", user_input)
+        else:
+            self.response.out.write("Thanks!")
+
+
+class TestHandler(webapp2.RequestHandler):
+    def post(self):
+        comment = self.request.get("comment")
+        self.response.out.write(comment)
+
+        #self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.out.write(self.request)
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/testform', TestHandler)],
+                              debug=True)
